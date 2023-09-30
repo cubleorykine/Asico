5c3ad1fbb279d1912156b3a353012f3c1f145c83
cubleorykine <137178888+cubleorykine@users.noreply.github.com>
2023-09-30 08:06:41 +0700

    Create Example.lua

---
+++ Example.lua
@@ -0,0 +1,10 @@
+-- for roblox.
+local cppBuild = require(yourmodulepath)
+cppBuild.service('anything')
+cppBuild.package('main')
+
+int = {
+	cppBuild.using('dxf')
+}
+
+cppBuild.cout('cout << Hello World!')
